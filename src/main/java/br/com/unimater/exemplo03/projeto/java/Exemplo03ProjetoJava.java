3067f985126807913511ba2f656bdc1166577c61
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-05-06 21:29:31 -0300

    Primeiro commit

---
+++ src/main/java/br/com/unimater/exemplo03/projeto/java/Exemplo03ProjetoJava.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.unimater.exemplo03.projeto.java;
+
+/**
+ *
+ * @author cezar
+ */
+public class Exemplo03ProjetoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Olá pessoal");
+    }
+}
