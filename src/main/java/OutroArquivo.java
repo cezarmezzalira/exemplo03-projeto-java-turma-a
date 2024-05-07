c7e0154686b25ab67200cc4e927b344cb357c010
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-05-06 21:50:31 -0300

    Criação Outro Arquivo

---
+++ src/main/java/OutroArquivo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author cezar
+ */
+public class OutroArquivo {
+    public static void main(String[] args){
+        System.out.println("Olá pessoal");
+        int valor = 0;
+        System.out.println(valor);
+    }
+}   
