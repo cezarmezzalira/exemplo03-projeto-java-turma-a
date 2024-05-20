2e5436340845b1ddd519a189d683847b0be92421
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-05-20 19:39:22 -0300

    Outra alteração qualquer

--- src/main/java/Pessoa.java
+++ src/main/java/Pessoa.java
@@ -1,5 +1,6 @@
 public class Pessoa {
     public static void main(String[] args) {
-	System.out.println("Meu nome é Fulano.");
+        System.out.println("Meu nome é Fulano.");
+        System.out.println("Bug de conflito");
     }
 }
