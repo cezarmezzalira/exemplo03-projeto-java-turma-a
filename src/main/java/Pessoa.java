250c0b731cd8a78add82b06e305dda3d63577bbd
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-05-20 23:05:48 +0000

    Alteração do dia a dia

--- src/main/java/Pessoa.java
+++ src/main/java/Pessoa.java
@@ -1,8 +1,6 @@
 public class Pessoa {
     public static void main(String[] args) {
         System.out.println("Meu nome é Fulano.");
-        System.out.println("Bug de conflito");
-
         System.out.println("Tudo OK");
     }
 }
