a872d23c649da59cc4468c5a073b75156415b41b
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2024-05-27 21:06:35 -0300

    Criada nova funcionalidade

---
+++ src/main/OutraFuncionalidade.java
@@ -0,0 +1,8 @@
+public class OutraFuncionalidade {
+    public static void main(String[] args) {
+        System.out.println("Olá pessoal");
+        int valor = 0;
+        valor = 1;
+        System.out.println("O valor é: " + valor);
+    }
+}
